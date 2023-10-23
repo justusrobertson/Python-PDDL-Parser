f24aab6a838b7433816459cb0695fefcba19a421
NarraAtor <eff3587@rit.edu>
2023-10-23 15:43:31 -0400

    Preconditions are now no longer required for the Action constructor. Needs to be revised to inherit bindings from Action class

--- Action.py
+++ Action.py
@@ -2,21 +2,29 @@ from Predicate import *
 
 
 class Action:
-    def __init__(self, name, parameters, binding_values, preconditions):
+    def __init__(self, name, parameters):
         self.name = name
         self.parameters = parameters
         self.bindings = HashTable(len(parameters))
         i = 0
         for parameter in self.parameters:
-            self.bindings.set_val(parameter, binding_values[i])
+            self.bindings.set_val(parameter, f"?{parameter}")
             i += 1
-        self.preconditions = preconditions
+        self.preconditions = []
 
 
-preconditionList = [Predicate("at", ["mover", "location"], ["arthur", "forest"])]
-effectsList = [Predicate("at", ["mover", "location"], ["arthur", "forest"])]
-newAction = Action("move", ["mover", "oldLoc", "newLoc"], ["arthur", "forest", "lake"], preconditionList)
+    # precondition should be a Predicate
+    def add_precondition(self, precondition):
+        self.preconditions.append(precondition)
+    def set_binding(self, parameter, binding_value):
+        self.bindings.set_val(parameter, binding_value)
+        for predicate in self.preconditions:
+            predicate.set_binding(parameter, binding_value)
 
+
+effectsList = [Predicate("at", ["mover", "location"])]
+newAction = Action("move", ["mover", "oldLoc", "newLoc"])
+newAction.add_precondition(Predicate("at", ["mover", "location"]))
 print(newAction.parameters)
 print(newAction.bindings)
-
+print(newAction.preconditions[0])

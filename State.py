5b673339c43dca476b28f369ec586fc40750efd8
NarraAtor <eff3587@rit.edu>
2023-11-08 12:40:22 -0500

    we can now check enabled actions from the world state!

--- State.py
+++ State.py
@@ -11,11 +11,38 @@ class State:
         for literal in literals:
             self.state_dictionary.set_val(f"{literal.to_string_ignoring_negation()}", not literal.is_negated)
 
+    def check_fully_bound_actions(self, possible_actions):
+        enabled_actions = []
+        for possible_action in possible_actions:
+            if not possible_action.is_fully_bound:
+                print(f"Action {possible_action} was not fully bound")
+                return
+            in_state = True
+            for precondition in possible_action.preconditions:
+                if not self.precondition_in_state(precondition):
+                    in_state = False
+                    break
+            if in_state:
+                enabled_actions.append(possible_action)
+
+        return enabled_actions
+
+
+
+    def precondition_in_state(self, precondition):
+        # my hash map implementation has no contains key method. I should refactor things in the future.
+        is_enabled = self.state_dictionary.get_val(precondition.to_string_ignoring_negation())
+        if is_enabled == "No record found":
+            return False
+        return is_enabled
+
     # action_templates is a list of Actions
-    def get_possible_actions(self, pddl_objects, action_templates, set_of_binds):
+    def get_possible_actions(self, pddl_objects, action_templates):
         possible_actions = []
         for action in action_templates:
-            self.compute_action_binds(pddl_objects, action, set_of_binds)
+            self.compute_action_binds(pddl_objects, action, possible_actions)
+
+        return possible_actions
 
     # current action is an action that's initially unbound.
     def compute_action_binds(self, objects, current_action, set_of_binds):
@@ -23,11 +50,12 @@ class State:
             for pddl_object in objects:
                 copy_action = Action("", "")
                 copy_action.__copy__(current_action)
-                # find the first unbound parameter the break
+                # find the first unbound parameter then break
                 for parameter in copy_action.parameters:
                     if copy_action.bindings.get_val(parameter).__contains__("?"):
                         copy_action.set_binding(parameter, pddl_object)
                         self.compute_action_binds(objects, copy_action, set_of_binds)
+                        break
 
         else:
             set_of_binds.append(current_action)
@@ -66,12 +94,17 @@ action_list[1].add_effect(Predicate("character", ["postTransformCharacter"], Fal
 
 new_state = State(predicate_list)
 print(new_state)
-set_of_new_bindings = []
-new_state.get_possible_actions(object_list, action_list, set_of_new_bindings)
+# set_of_new_bindings = []
+# new_state.get_possible_actions(object_list, action_list, set_of_new_bindings)
 # new_state.compute_action_binds(object_list, action_list[1], set_of_new_bindings)
 
-for fully_bound_action in set_of_new_bindings:
+print("fully bound actions:\n")
+for fully_bound_action in new_state.get_possible_actions(object_list, action_list):
     print(fully_bound_action)
+
+print("enabled actions:\n")
+for enabled_action in new_state.check_fully_bound_actions(new_state.get_possible_actions(object_list, action_list)):
+    print(enabled_action)
 # print(predicate_list[0].to_string_ignoring_negation())
 # print(new_state.state_dictionary.get_val(f"{predicate_list[0].to_string_ignoring_negation()}"))
 # print(new_state.state_dictionary.get_val(f"{predicate_list[1].to_string_ignoring_negation()}"))
